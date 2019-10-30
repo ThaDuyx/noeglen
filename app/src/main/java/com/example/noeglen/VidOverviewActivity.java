f9dd653f48fd1227dd17fa24a58a8375845e917f
Duyx <sandersen98@live.dk>
2019-10-30 16:42:54 +0100

    one mo' time

--- app/src/main/java/com/example/noeglen/VidOverviewActivity.java
+++ app/src/main/java/com/example/noeglen/VidOverviewActivity.java
@@ -2,13 +2,27 @@ package com.example.noeglen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class VidOverviewActivity extends AppCompatActivity {
+public class VidOverviewActivity extends AppCompatActivity implements View.OnClickListener {
 
+
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vidoverview);
+
+        btn.findViewById(R.id.button3);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, VidPageActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/noeglen/VidPageActivity.java
+++ app/src/main/java/com/example/noeglen/VidPageActivity.java
@@ -3,12 +3,22 @@ package com.example.noeglen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class VidPageActivity extends AppCompatActivity {
+public class VidPageActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vid_page);
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
